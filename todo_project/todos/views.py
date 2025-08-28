52303354c938e29a334aed023d33b4c0cecd5382
James <mirageapache@gmail.com>
2025-08-28 22:03:25 +0800

    [feat]編輯Todo / 刪除Todo / 新增路由[/edit] 和 [/delete] / 建立edit_todo和delete_todo模板 / 清單頁加上編輯&刪除連結

--- todo_project/todos/views.py
+++ todo_project/todos/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import redirect, render
 from .models import Todo
 from .forms import TodoForm
+from django.shortcuts import get_object_or_404
 
+# 待辦清單
 def todo_list(request):
   todos = Todo.objects.all().order_by('-created_at')
   return render(request, 'todos/index.html', {'todos': todos})
 
+# 新增待辦事項
 def add_todo(request):
   # POST 請求 => 驗證表單並儲存
   if request.method == 'POST':
@@ -17,3 +20,23 @@ def add_todo(request):
   else:
     form = TodoForm()
   return render(request, 'todos/add_todo.html', {'form': form})
+
+# 編輯待辦事項
+def edit_todo(request, todo_id):
+  todo = get_object_or_404(Todo, id=todo_id)   # 找不到指定的todo，回傳404
+  if request.method == 'POST':
+    form = TodoForm(request.POST, instance=todo)
+    if form.is_valid():
+      form.save()
+      return redirect('todo_list')
+  else:
+    form = TodoForm(instance=todo)   # 把現有資料帶入表單(編輯模式)
+  return render(request, 'todos/edit_todo.html', {'form': form, 'todo': todo})
+
+# 刪除待辦事項
+def delete_todo(request, todo_id):
+  todo = get_object_or_404(Todo, id=todo_id)
+  if request.method == 'POST':
+    todo.delete()
+    return redirect('todo_list')
+  return render(request, 'todos/delete_todo.html', {'todo': todo})
